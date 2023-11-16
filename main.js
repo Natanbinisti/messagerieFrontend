b0d2fa6e03e197f14cbae22ed5b5d9b179cb4d63
Natan Binisti <natanbinistilyon@gmail.com>
2023-11-16 21:52:57 +0100

    commit

--- main.js
+++ main.js
@@ -1,71 +1,180 @@
-
-
-const baseUrl = "https://b1messenger.imatrythis.tk/"
-
- const corpsRequeteRegister = {
-     username:"natan",
-     password : "tropbonlapizza"
- }
- const registerParams = {
-         method : 'POST',
-         headers : {"Content-type":"application/json"},
-         body : JSON.stringify(corpsRequeteRegister)
-
- }
-
- fetch(`${baseUrl}register`, registerParams)
-     .then(response=>response.json())
-     .then(data=> console.log(data))
-
-
-
-const corpsRequeteLogin = {
-    username:"natan",
-     password : "tropbonlapizza"
- }
-
- const loginParams = {
-         method : 'POST',
-         headers : {"Content-type":"application/json"},
-         body : JSON.stringify(corpsRequeteLogin)
-
- }
-
- fetch(`${baseUrl}login`, loginParams)
-     .then(response=>response.json())
-     .then(data=> console.log(data))
-const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE2OTk5NzMzNjUsImV4cCI6MTY5OTk3Njk2NSwicm9sZXMiOlsiUk9MRV9VU0VSIl0sInVzZXJuYW1lIjoibmF0YW4ifQ.ZRRtiIrooZGbrMthXWaOxsqYaP2QSaWjWEzDfawD-3dKRDoetJVQ5YShUYMYUTi3pViukjj9NVLnmwKcUdiZo866VxwpryeopR6u8UoPBlUtfGQQ4TkYsigvfsN8H_TxzAfKiR73AOx10Unh0VHBtdMhUbF7-g4rco1P5OsrJ4KIepTc2SMUIJ96EHSyIrxKxsea30STogNZovVRz3DvkDzNeW7Moses8zWetAREw4K8OOkArSpdf2R1KCoQes6VPRSWF_ShA1KBggsD-tg4nmziWuS19wOisB055xfNTrXO9BIyDAx3Wsxm8V1vZwrlT_tpBWg8q-KLH19grnKV2g"
-
-
-const messagesListParams = {
-    method : 'GET',
-    headers : {"Content-type":"application/json",
-        "Authorization":`Bearer ${token}`},
-
-
-}
-
 const content = document.querySelector('.content')
+const refreshButton = document.querySelector('#refreshButton')
 
-async function getMessages()
+let token = null
+
+refreshButton.addEventListener('click', ()=>{
+    run()
+})
+
+
+
+function run()
 {
-    await fetch(`${baseUrl}api/messages`, messagesListParams)
-        .then(response=>response.json())
-        .then(data=>{
-            return data
+    if(!token){
+        renderLoginForm()
+    }else{
+        fetchMessages().then(messages=>{
+            renderMessages(messages)
+        })
 
-        } )
+    }
 }
-function passeLesMEssagesJeLesMEtsDansUnTemplate(messages)
+
+function renderLoginForm()
 {
-    messages.forEach((message)=>{
-        content.innerHTML+= `<p>${message.content}</p>`
+    let loginTemplate = `<div class="login form-control">
+                            <h2>Login form</h2>
+                            <input type="text" id="username" class="form-control" placeholder="username">
+                            <input type="password" name="" id="password" class="form-control" placeholder="password">
+                            <button class="btn btn-success" id="loginButton">Log in</button>
+                        </div>`
+
+
+    render(loginTemplate)
+    const loginButton = document.querySelector('#loginButton')
+    loginButton.addEventListener('click', ()=>{
+        login()
     })
+
+
+}
+function login()
+{
+    const username = document.querySelector('#username')
+    const password = document.querySelector('#password')
+
+    let body = {
+        username : username.value,
+        password : password.value
+    }
+
+    let params = {
+        headers : {"Content-type":"application/json"},
+        method : "POST",
+        body : JSON.stringify(body)
+    }
+
+
+    fetch('https://b1messenger.imatrythis.tk/login', params)
+        .then(response=>response.json())
+        .then(data =>{
+            if(data.message == "Invalid credentials.")
+            {
+                renderLoginForm()
+            }else{
+                token = data.token
+                run()
+            }
+
+
+        })
+
+}
+function generateMessage(message)
+{
+    let messageTemplate = `    <div class="row">
+                                    <p><strong>${message.author.username} :</strong> ${message.content}  </p>
+                                <hr>
+                            </div>`
+
+    return messageTemplate
 }
 
-let bouton = document.querySelector()
-//bouton.addEventListener('click', ()=>{
-  //  getMessages().then(messages =>{
-   //     passeLesMEssagesJeLesMEtsDansUnTemplate(messages)
-   // })
-//})
+function renderMessages(tableauMessages)
+{
+    let contentMessages=""
+
+    tableauMessages.forEach(message=>{
+
+        contentMessages += generateMessage(message)
+    })
+
+    let messagesAndMessageForm = contentMessages + generateMessageForm()
+
+    render(messagesAndMessageForm)
+
+    const postMessage = document.querySelector('#postMessage')
+    const postMessageButton = document.querySelector('#postMessageButton')
+
+    postMessageButton.addEventListener('click', ()=>{
+        sendMessage(postMessage.value)
+    })
+
+}
+
+function render(pageContent)
+{
+    content.innerHTML = ""
+    content.innerHTML = pageContent
+}
+
+async function fetchMessages()
+{
+
+    let params = {
+        headers : {"Content-type":"application/json",
+            "Authorization":`Bearer ${token}`},
+        method : "GET"
+    }
+
+
+    return await fetch('https://b1messenger.imatrythis.tk/api/messages', params)
+        .then(response=>response.json())
+        .then(data =>{
+            if(data.message == "Invalid credentials.")
+            {
+                renderLoginForm()
+            }else{
+                return data
+            }
+
+
+        })
+}
+
+function generateMessageForm()
+{
+    let messageFormTemplate = `<div class="form-control">
+                                    <input class="form-control" type="text" name="" id="postMessage" placeholder="your message">
+                                    <button class="btn btn-success form-control" id="postMessageButton">Envoyer</button>
+                                </div>`
+    return messageFormTemplate
+}
+
+function sendMessage(messageToSend)
+{
+    let body = {
+        content:messageToSend
+    }
+
+    let params = {
+        headers : {"Content-type":"application/json", "Authorization": `Bearer ${token}`},
+        method : "POST",
+        body : JSON.stringify(body)
+    }
+
+
+    fetch('https://b1messenger.imatrythis.tk/api/messages/new', params)
+        .then(response=>response.json())
+        .then(data =>{
+            if(data.message == "Invalid credentials.")
+            {
+                renderLoginForm()
+            }else{
+                if(data == "OK"){
+                    run()
+                }else{
+                    alert('problem')
+                    run()
+                }
+            }
+        })
+}
+let modify = {
+   headers: {"Content-type":"application/json", "Authorization": `Bearer ${token}`},
+    method : "PUT",
+}
+//fetch(`https://b1messenger.imatrythis.tk/api/messages/${data.id}/edit`)
+
+run()
